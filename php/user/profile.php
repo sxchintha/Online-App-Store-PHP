d319bf6b1b4d754df1bc83293a86e855631db994
Sachintha Gunaratne <sachinthasl99@gmail.com>
2022-05-09 09:47:29 +0530

    initial commit

---
+++ php/user/profile.php
@@ -0,0 +1,20 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/online-app-store-php/php/components/header.php';
+include $_SERVER['DOCUMENT_ROOT'].'/online-app-store-php/php/components/navigation.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
