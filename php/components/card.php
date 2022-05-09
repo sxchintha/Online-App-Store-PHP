bf95a3e41b2f6180f5b18e4b7846099cb1e51165
Akila Kavinda <akilakavinda909@gmail.com>
2022-05-09 23:20:28 +0530

    card and form template

---
+++ php/components/card.php
@@ -0,0 +1,13 @@
+<style>
+    <?php
+    include $_SERVER['DOCUMENT_ROOT'] . '/online-app-store-php/css/card.css';
+    ?>
+</style>
+
+
+<div class="card">
+      <img src="../../img/app.jpg" alt="Avatar" style="width: 100%" class="img" />
+      <div class="container">
+        <h4 class="card-title"><b>App Name</b></h4>
+        <p>Architect & Engineer</p>
+      </div>
